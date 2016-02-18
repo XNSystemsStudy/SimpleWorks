244402e8a1a6c495859e74fc17eb90988ce8a962
심다래 <sdr2131@dongwon-gwak-ui-macbook-air.local>
2016-02-18 10:25:37 +0900

    Initial Commit

---
+++ SimpleWorks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleWorks
+//
+//  Created by 심다래 on 2016. 2. 18..
+//  Copyright © 2016년 XNRND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
